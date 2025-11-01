cb3ba4b68a1a4f5a3b4670537044393403a585ad
Pasachiya <pasachiya@gmail.com>
2025-11-01 21:43:13 +0530

    css fix 2

---
+++ next.config.js
@@ -0,0 +1,11 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: 'export',
+  basePath: '/dpv-offshore-redesign-2',
+  assetPrefix: '/dpv-offshore-redesign-2/',
+  images: {
+    unoptimized: true,
+  },
+}
+
+module.exports = nextConfig
