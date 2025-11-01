7d7a08f392ab0c264e839deca51088bec72422b7
Pasachiya <pasachiya@gmail.com>
2025-11-01 21:48:53 +0530

    css fix 4

--- next.config.js
+++ next.config.js
@@ -1,11 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
-  basePath: '/dpv-offshore-redesign-2',
+  basePath: '/dpv-offshore-redesign-2', // Must match your GitHub repo name exactly
   assetPrefix: '/dpv-offshore-redesign-2/',
   images: {
     unoptimized: true,
   },
+  trailingSlash: true,
 }
 
 module.exports = nextConfig
